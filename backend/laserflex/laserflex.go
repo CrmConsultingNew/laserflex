033f98d30b76fe60a211caf0ff49e55dd00af5f3
unknown <andreevivan8891@gmail.com>
2024-11-18 21:21:52 +0300

    just tests works

--- backend/laserflex/laserflex.go
+++ backend/laserflex/laserflex.go
@@ -1,32 +1,20 @@
 package laserflex
 
 import (
+	"io"
 	"log"
 	"net/http"
 )
 
 func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 	log.Println("Connection is starting...")
-
-	// Извлечение параметров из строки запроса
-	query := r.URL.Query()
-
-	// Читаем параметры
-	doc := query.Get("doc")                       // Параметр doc (ссылка на файл)
-	userId := query.Get("userId")                 // Параметр userId
-	clientID := query.Get("client_id")            // Параметр client_id
-	clientSecret := query.Get("client_secret")    // Параметр client_secret
-	smartProcessID := query.Get("smartProcessID") // Параметр smartProcessID
-
-	// Логирование параметров
-	log.Println("Parsed Query Params:")
-	log.Printf("doc: %s\n", doc)
-	log.Printf("userId: %s\n", userId)
-	log.Printf("client_id: %s\n", clientID)
-	log.Printf("client_secret: %s\n", clientSecret)
-	log.Printf("smartProcessID: %s\n", smartProcessID)
-
-	// Ответ сервером
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte("Query parameters received and processed successfully."))
+	// Чтение тела запроса
+	bs, err := io.ReadAll(r.Body)
+	if err != nil {
+		log.Println("error reading request body:", err)
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+	defer r.Body.Close()
+	log.Println("FULL REQUEST CONNECTION: ", string(bs))
 }
