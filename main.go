d34fad877e54dfa28cf949ef4cf16dff03aeb273
unknown <andreevivan8891@gmail.com>
2024-12-18 23:57:49 +0300

    just tests works

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bitrix_app/backend/bitrix/endpoints"
-	"bitrix_app/backend/laserflex"
 	"bitrix_app/backend/routes"
 	"fmt"
 	"github.com/joho/godotenv"
@@ -22,8 +21,7 @@ func main() {
 	/*laserflex.ReadXlsProducts("file_downloaded_xls1.xlsx")
 	laserflex.ReadXlsProductRow("file_downloaded_xls1.xlsx")
 	return*/
-	laserflex.ParseSheetForColorColumnAndTasks("file.xlsx")
-	return
+
 	fmt.Println("service starting...")
 
 	// Загрузка переменных окружения из файла .env
