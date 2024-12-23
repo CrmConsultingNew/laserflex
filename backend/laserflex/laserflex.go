50f7b58fd37d20fea619015d3181c9ca1d947128
unknown <andreevivan8891@gmail.com>
2024-12-24 00:57:04 +0300

    just tests works

--- backend/laserflex/laserflex.go
+++ backend/laserflex/laserflex.go
@@ -12,52 +12,6 @@ import (
 	"time"
 )
 
-// Структуры для задачи
-type Task struct {
-	Title         string   `json:"TITLE"`
-	ResponsibleID int      `json:"RESPONSIBLE_ID"`
-	GroupID       int      `json:"GROUP_ID,omitempty"`
-	UfCrmTask     []string `json:"UF_CRM_TASK,omitempty"`
-}
-
-// Структура для создания задачи с полями
-type TaskWithParent struct {
-	Title         string           `json:"TITLE"`
-	ResponsibleID int              `json:"RESPONSIBLE_ID"`
-	GroupID       int              `json:"GROUP_ID,omitempty"`
-	ParentID      int              `json:"PARENT_ID"`
-	CustomFields  CustomTaskFields `json:"custom_fields,omitempty"`
-}
-
-// Структура для общего тела запроса
-type TaskRequest struct {
-	Fields map[string]interface{} `json:"fields"`
-}
-
-type TaskResponse struct {
-	Result struct {
-		Task struct {
-			ID json.RawMessage `json:"id"`
-		} `json:"task"`
-	} `json:"result"`
-}
-
-// Структура для пользовательских полей задачи
-type CustomTaskFields struct {
-	Quantity          string `json:"UF_AUTO_552243496167,omitempty"` // Кол-во
-	TemporaryOrderSum string `json:"UF_AUTO_555642596740,omitempty"` // Временная сумма заказа
-	OrderNumber       string `json:"UF_AUTO_303168834495,omitempty"` // № заказа
-	Customer          string `json:"UF_AUTO_876283676967,omitempty"` // Заказчик
-	Manager           string `json:"UF_AUTO_794809224848,omitempty"` // Менеджер
-	Material          string `json:"UF_AUTO_468857876599,omitempty"` // Материал
-	Bend              string `json:"UF_AUTO_726724682983,omitempty"` // Гибка
-	ProductionTask    string `json:"UF_AUTO_433735177517,omitempty"` // Произв. Задача
-	Comment           string `json:"UF_AUTO_497907774817,omitempty"` // Комментарий
-	Coating           string `json:"UF_AUTO_512869473370,omitempty"` // Покрытие
-	AllowTimeTracking string `json:"ALLOW_TIME_TRACKING,omitempty"`
-	TimeEstimate      int    `json:"TIME_ESTIMATE,omitempty"` //
-}
-
 func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 	//order_number={{№ заказа}}&deadline={{Срок сдачи}}
 	log.Println("Connection is starting...")
@@ -67,6 +21,8 @@ func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 	fileID := queryParams.Get("file_id")
 	smartProcessIDStr := queryParams.Get("smartProcessID")
 	orderNumber := queryParams.Get("order_number")
+	engineerStr := queryParams.Get("engineer_id")
+	engineerId, _ := strconv.Atoi(engineerStr)
 
 	// 1
 
@@ -189,7 +145,7 @@ func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 		arrayOfTasksIDsPipeCutting = append(arrayOfTasksIDsPipeCutting, taskIDs...)
 	}
 
-	if taskIDs, err := processProducts(fileName, smartProcessID, 149); err == nil {
+	if taskIDs, err := processProducts(fileName, smartProcessID, engineerId); err == nil {
 		arrayOfTasksIDsProducts = append(arrayOfTasksIDsProducts, taskIDs)
 	}
 
@@ -217,7 +173,7 @@ func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 	if CheckCoatingColumn(fileName) {
 		// Если есть данные, получаем цвета из "Цвет/цинк"
 		colors := ParseSheetForColorColumn(fileName)
-		_, err := AddTaskToGroupColor(orderNumber, "Проверить наличие ЛКП на складе в ОМТС", 149, 12, 1046, smartProcessID, colors)
+		_, err := AddTaskToGroupColor(orderNumber, "Проверить наличие ЛКП на складе в ОМТС", engineerId, 12, 1046, smartProcessID, colors)
 		if err != nil {
 			log.Printf("Error creating task with colors: %v", err)
 			http.Error(w, "Failed to create task with colors", http.StatusInternalServerError)
@@ -226,7 +182,7 @@ func LaserflexGetFile(w http.ResponseWriter, r *http.Request) {
 
 	} else {
 		// Если данных нет, создаём задачу без цветов
-		_, err := AddTaskToGroupColor(orderNumber, "Задача в ОМТС с материалами из накладной", 149, 12, 1046, smartProcessID, nil)
+		_, err := AddTaskToGroupColor(orderNumber, "Задача в ОМТС с материалами из накладной", engineerId, 12, 1046, smartProcessID, nil)
 		if err != nil {
 			log.Printf("Error creating task without colors: %v", err)
 			http.Error(w, "Failed to create task without colors", http.StatusInternalServerError)
